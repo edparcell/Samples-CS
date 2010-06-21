ef2643dbe411afe8593c25047eb32e6a3b4580ea
edparcell <edparcell@gmail.com>
2010-06-21 21:54:00 +0100

    Example call to AES.Decipher.

--- AES/Main.cs
+++ AES/Main.cs
@@ -6,7 +6,10 @@ namespace AES
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string cipherText = "U2FsdGVkX19IHNsaQjSutr7BJDWPMWzkAtnsYi3olqDg64l2F5DxSCa4QJB7A4vx";
+			string password = "password";
+			string plainText = AES.Decipher(cipherText, password);
+			Console.WriteLine ("{0} -> {1}", cipherText, plainText);
 		}
 	}
 }
