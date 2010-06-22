67c0e454a01e8cbfcdcf058768e946b60ad006f1
edparcell <edparcell@gmail.com>
2010-06-22 01:16:14 +0100

    Example round-trips a string.

--- AES/Main.cs
+++ AES/Main.cs
@@ -6,10 +6,13 @@ namespace AES
 	{
 		public static void Main (string[] args)
 		{
-			string cipherText = "U2FsdGVkX19IHNsaQjSutr7BJDWPMWzkAtnsYi3olqDg64l2F5DxSCa4QJB7A4vx";
+			string originalText = "Hello, secure world!";
 			string password = "password";
+			string cipherText = AES.Encipher(originalText, password, true);
+			//string cipherText = "U2FsdGVkX19IHNsaQjSutr7BJDWPMWzkAtnsYi3olqDg64l2F5DxSCa4QJB7A4vx";
+
 			string plainText = AES.Decipher(cipherText, password);
-			Console.WriteLine ("{0} -> {1}", cipherText, plainText);
+			Console.WriteLine ("{0} -> {1} -> {2}", originalText, cipherText, plainText);
 		}
 	}
 }
