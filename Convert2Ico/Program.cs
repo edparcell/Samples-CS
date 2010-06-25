ff9473f6163e4a3173e277a05586f9d3616ddebb
Ed Parcell <ed.parcell@cobaltquantware.com>
2010-06-25 16:08:47 +0100

    Added Convert2Ico example of converting from a PNG file to an ICO file.

---
+++ Convert2Ico/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace Convert2Ico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFilename = args[0];
+            string outFilename = args[1];
+            Bitmap bitmap = new Bitmap(inFilename);
+            IconConverter convertor = new IconConverter();
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+            FileStream fs = new FileStream(outFilename, FileMode.Create);
+            icon.Save(fs);
+            fs.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
