f9beb7ef047dee3df4a35e985afc57fc68419a01
Ed Parcell <ed.parcell@cobaltquantware.com>
2010-06-25 18:41:56 +0100

    Added NamedPipe examples.

---
+++ NamedPipeClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Pipes;
+
+namespace NamedPipeClient
+{
+    class Program
+    {
+        public const string PIPE_NAME = @"\\.\pipe\examplePipe";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opening pipe {0}", PIPE_NAME);
+            NamedPipeClientStream pipeClient = new NamedPipeClientStream(
+                ".",
+                PIPE_NAME,
+                PipeDirection.Out,
+                PipeOptions.Asynchronous
+                );
+            pipeClient.Connect(10000);
+
+            Console.WriteLine("Writing message.");
+
+            string messageText = "Hello World";
+            byte[] message = Encoding.UTF8.GetBytes(messageText);
+            pipeClient.Write(message, 0, message.Length);
+
+            string messageText2 = "This is a second message";
+            byte[] message2 = Encoding.UTF8.GetBytes(messageText2);
+            pipeClient.Write(message2, 0, message2.Length);
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ NamedPipeServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Pipes;
+using System.IO;
+
+namespace NamedPipeServer
+{
+    class Program
+    {
+        public const string PIPE_NAME = @"\\.\pipe\examplePipe";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating pipe {0}", PIPE_NAME);
+            NamedPipeServerStream pipeServer = new NamedPipeServerStream(
+                PIPE_NAME,
+                PipeDirection.In,
+                10,
+                PipeTransmissionMode.Message,
+                PipeOptions.None
+                );
+
+            Console.WriteLine("Waiting for connection.");
+            pipeServer.WaitForConnection();
+
+            Console.WriteLine("Client connected.");
+
+            int bytes_read;
+            while(true)
+            {
+                Byte[] buffer = new Byte[4096];
+                StringBuilder sb = new StringBuilder();
+                do
+                {
+                    bytes_read = pipeServer.Read(buffer, 0, buffer.Length);
+                    sb.Append(Encoding.UTF8.GetChars(buffer, 0, bytes_read));
+                    if (pipeServer.IsMessageComplete)
+                    {
+                        break;
+                    }
+                }
+                while (!pipeServer.IsMessageComplete);
+
+                if (bytes_read == 0)
+                {
+                    Console.WriteLine("Received empty message, quitting.");
+                    break;
+                }
+                
+                Console.WriteLine("Message received: {0}", sb.ToString());
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
